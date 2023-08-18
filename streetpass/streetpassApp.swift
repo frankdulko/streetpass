71584ef51621a815561934f8f85236fad8568949
frankdulko <79063207+frankdulko@users.noreply.github.com>
2023-08-18 10:46:52 -0400

    Initial Commit

---
+++ streetpass/streetpassApp.swift
@@ -0,0 +1,17 @@
+//
+//  streetpassApp.swift
+//  streetpass
+//
+//  Created by Frank Dulko on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct streetpassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
